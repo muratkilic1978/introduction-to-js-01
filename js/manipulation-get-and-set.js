339247841c6d6b20ccff611d03fa0d7697262ceb
muratkilic1978 <muratkilic1978@gmail.com>
2020-01-28 17:41:28 +0100

    script.js was updated with solution

---
+++ js/manipulation-get-and-set.js
@@ -0,0 +1,7 @@
+// fetch the DOM element that has the ID attribute greeting andd store it in the const variable
+const greetingElem = document.querySelector("#greeting");
+
+// innerHTML sets the value and apply any HTML formatting
+greetingElem.innerHTML = "<strong>Mr. King</strong>";
+
+
